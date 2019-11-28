335978d76837a626bc14239fa6dcf6f3502ca2c4
Vincent Maverick S Durano <vmsdurano@live.com.ph>
2019-11-27 19:05:35 -0600

    [FET] Add more unit test examples

--- ApiBoilerPlate.Test/v1/PersonsControllerTests.cs
+++ ApiBoilerPlate.Test/v1/PersonsControllerTests.cs
@@ -3,6 +3,7 @@ using ApiBoilerPlate.Contracts;
 using ApiBoilerPlate.Data.Entity;
 using ApiBoilerPlate.DTO.Response;
 using ApiBoilerPlate.DTO.Request;
+using ApiBoilerPlate.Infrastructure.Configs;
 using AutoMapper;
 using AutoWrapper.Wrappers;
 using Microsoft.Extensions.Logging;
@@ -18,17 +19,19 @@ namespace ApiBoilerPlate.Test.v1
     public class PersonsControllerTests
     {
         private readonly Mock<IPersonManager> _mockDataManager;
-        private readonly Mock<IMapper> _mapper;
         private readonly PersonsController _controller;
 
         public PersonsControllerTests()
         {
             var logger = Mock.Of<ILogger<PersonsController>>();
 
-            _mapper = new Mock<IMapper>();
+            var mapperProfile = new MappingProfileConfiguration();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
+            var mapper = new Mapper(configuration);
+
             _mockDataManager = new Mock<IPersonManager>();
 
-            _controller = new PersonsController(_mockDataManager.Object, _mapper.Object, logger);
+            _controller = new PersonsController(_mockDataManager.Object, mapper, logger);
         }
 
         private IEnumerable<Person> GetFakePersonLists()
@@ -52,6 +55,44 @@ namespace ApiBoilerPlate.Test.v1
             };
         }
 
+        private CreatePersonRequest FakeCreateRequestObject()
+        {
+            return new CreatePersonRequest()
+            {
+                FirstName = "Vinz",
+                LastName = "Durano",
+                DateOfBirth = Convert.ToDateTime("02/15/2016")
+            };
+        }
+
+        private UpdatePersonRequest FakeUpdateRequestObject()
+        {
+            return new UpdatePersonRequest()
+            {
+                FirstName = "Vinz",
+                LastName = "Durano",
+                DateOfBirth = Convert.ToDateTime("02/15/2016")
+            };
+        }
+
+        private CreatePersonRequest FakeCreateRequestObjectWithMissingAttribute()
+        {
+            return new CreatePersonRequest()
+            {
+                FirstName = "Vinz",
+                LastName = "Durano"
+            };
+        }
+
+        private CreatePersonRequest FakeUpdateRequestObjectWithMissingAttribute()
+        {
+            return new CreatePersonRequest()
+            {
+                FirstName = "Vinz",
+                LastName = "Durano"
+            };
+        }
+
         [Fact]
         public async Task GET_All_RETURNS_OK()
         {
@@ -64,7 +105,7 @@ namespace ApiBoilerPlate.Test.v1
             var result = await _controller.Get();
 
             // Assert
-            var persons = Assert.IsType<List<Person>>(result);
+            var persons = Assert.IsType<List<PersonResponse>>(result);
             Assert.Equal(2, persons.Count);
         }
 
@@ -77,59 +118,116 @@ namespace ApiBoilerPlate.Test.v1
                .ReturnsAsync(GetFakePersonLists().Single(p => p.ID.Equals(id)));
 
             var person = await _controller.Get(id);
-            Assert.IsType<Person>(person);
+            Assert.IsType<PersonResponse>(person);
         }
 
         [Fact]
         public async Task GET_ById_RETURNS_NOTFOUND()
         {
-            await Assert.ThrowsAsync<ApiException>(() => _controller.Get(1));
+            var apiException = await Assert.ThrowsAsync<ApiException>(() => _controller.Get(10));
+            Assert.Equal(404, apiException.StatusCode);
         }
 
         [Fact]
         public async Task POST_Create_RETURNS_BADREQUEST()
         {
-            // Arrange
-            var dobMissingAttribute = new CreatePersonRequest()
-            {
-                FirstName = "Vinz",
-                LastName = "Durano"
-            };
-
-            // Act
             _controller.ModelState.AddModelError("DateOfBirth", "Required");
 
-            // Assert
-
-            await Assert.ThrowsAsync<ApiException>(() => _controller.Post(dobMissingAttribute));
+            var apiException = await Assert.ThrowsAsync<ApiException>(() => _controller.Post(FakeCreateRequestObjectWithMissingAttribute()));
+            Assert.Equal(400, apiException.StatusCode);
         }
 
         [Fact]
         public async Task POST_Create_RETURNS_OK()
         {
-            // Arrange
-            var dto = new CreatePersonRequest()
-            {
-                FirstName = "Vinz",
-                LastName = "Durano",
-                DateOfBirth = Convert.ToDateTime("02/15/2016")
-            };
 
-            var personEntity = new Person()
-            {
-                FirstName = dto.FirstName,
-                LastName = dto.LastName,
-                DateOfBirth = dto.DateOfBirth
-            };
-
-            _mockDataManager.Setup(manager => manager.CreateAsync(personEntity))
+            _mockDataManager.Setup(manager => manager.CreateAsync(It.IsAny<Person>()))
                 .ReturnsAsync(It.IsAny<long>());
 
-            // Act
-            var person = await _controller.Post(dto);
+            var person = await _controller.Post(FakeCreateRequestObject());
 
-            // Assert
-            Assert.IsType<ApiResponse>(person);
+            var response = Assert.IsType<ApiResponse>(person);
+            Assert.Equal(201, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task POST_Create_RETURNS_SERVERERROR()
+        {
+            _mockDataManager.Setup(manager => manager.CreateAsync(It.IsAny<Person>()))
+                .Throws(new Exception());
+
+            await Assert.ThrowsAsync<Exception>(() => _controller.Post(FakeCreateRequestObject()));
+        }
+
+        [Fact]
+        public async Task PUT_ById_RETURNS_OK()
+        {
+            _mockDataManager.Setup(manager => manager.UpdateAsync(It.IsAny<Person>()))
+                 .ReturnsAsync(true);
+
+            var person = await _controller.Put(1, FakeUpdateRequestObject());
+
+            var response = Assert.IsType<ApiResponse>(person);
+            Assert.Equal(200, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PUT_ById_RETURNS_NOTFOUND()
+        {
+            var apiException = await Assert.ThrowsAsync<ApiException>(() => _controller.Put(10, FakeUpdateRequestObject()));
+            Assert.Equal(404, apiException.StatusCode);
+        }
+
+        [Fact]
+        public async Task PUT_ById_RETURNS_BADREQUEST()
+        {
+            _controller.ModelState.AddModelError("DateOfBirth", "Required");
+
+            var apiException = await Assert.ThrowsAsync<ApiException>(() => _controller.Put(10, FakeUpdateRequestObject()));
+            Assert.Equal(400, apiException.StatusCode);
+        }
+
+        [Fact]
+        public async Task PUT_ById_RETURNS_SERVERERROR()
+        {
+
+            _mockDataManager.Setup(manager => manager.UpdateAsync(It.IsAny<Person>()))
+                .Throws(new Exception());
+
+            await Assert.ThrowsAsync<Exception>(() => _controller.Put(10, FakeUpdateRequestObject()));
+        }
+
+        [Fact]
+        public async Task DELETE_ById_RETURNS_OK()
+        {
+            long id = 1;
+
+            _mockDataManager.Setup(manager => manager.DeleteAsync(id))
+                 .ReturnsAsync(true);
+
+            bool isDeleted = await _controller.Delete(id);
+
+            var response = Assert.IsType<bool>(isDeleted);
+
+            Assert.True(response);
+        }
+
+        [Fact]
+        public async Task DELETE_ById_RETURNS_NOTFOUND()
+        {
+            var apiException = await Assert.ThrowsAsync<ApiException>(() => _controller.Delete(1));
+            Assert.Equal(404, apiException.StatusCode);
+        }
+
+        [Fact]
+        public async Task DELETE_ById_RETURNS_SERVERERROR()
+        {
+            long id = 1;
+
+            _mockDataManager.Setup(manager => manager.DeleteAsync(id))
+                .Throws(new Exception());
+
+            await Assert.ThrowsAsync<Exception>(() => _controller.Delete(id));
         }
     }
 }
